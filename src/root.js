c8416081b6238c085c65c81f0fb7577c0b91c32b
jean-michel-legrand <defactojml@gmail.com>
2016-03-11 09:08:47 -0800

    Hello world with ES5 declaration

--- src/root.js
+++ src/root.js
@@ -1,1 +1,16 @@
-document.write('hello fluent san francisco')
+/* react hello world with ES5 declaration */
+
+var React = require('react');
+var ReactDom = require('react-dom');
+
+var Hello = React.createClass({
+  render: function () {
+    return <div> Hello world, Fluent US is done temp really </div>
+  }
+});
+
+
+ReactDom.render(<Hello/>, document.body.firstChild);
+
+
+
